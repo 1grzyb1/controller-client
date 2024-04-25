9cc92a376c9b6ef9dafc8b87d50a2f22949752c2
1grzyb1 <grzybol.k@gmail.com>
2024-04-25 12:17:56 +0200

    auto-configuration wip

---
+++ library/src/main/java/ovh/snet/grzybek/controller/client/core/ControllerClientAutoConfiguration.java
@@ -0,0 +1,9 @@
+package ovh.snet.grzybek.controller.client.core;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan(basePackages = "ovh.snet.grzybek.controller.client.core")
+public class ControllerClientAutoConfiguration {
+}
